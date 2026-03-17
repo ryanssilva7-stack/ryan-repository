df97c467f1f49a7a7927184864b0e62a16144d22
ryanssilva7-stack <ryan.s.silva7@ba.estudante.senai.br>
2026-03-17 16:50:33 -0300

    Add files via upload

---
+++ laco_while.py
@@ -0,0 +1,12 @@
+import os
+os.system("cls")
+
+while True:
+    idade = 7
+    if idade >= 18:
+        print("Acesso negado!")
+        print("Tente novamente.., \n")
+    else:
+        print("Acesso permitido.")
+        break
+    
