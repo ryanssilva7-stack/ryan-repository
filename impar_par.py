c16f88f70e246621b74e07a67556c6306363c708
ryanssilva7-stack <ryan.s.silva7@ba.estudante.senai.br>
2026-03-24 20:18:14 -0300

    impar_par.py

--- impar_par.py
+++ impar_par.py
@@ -5,6 +5,7 @@ os.system("cls")
 soma = 0
 par = 0
 impar = 0
+
 # processo
 for i in range (1, 6):
     numero = int(input(f"Digite o {i}º número: "))
@@ -13,4 +14,5 @@ for i in range (1, 6):
         par += 1
     else:
         impar += 1
+        
     print(f"Números pares: {par}; Números impares: {impar}\n")
