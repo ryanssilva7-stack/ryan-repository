73e5e9182e716ebb6c4c97239e26e4d9ed3f1cda
ryanssilva7-stack <ryan.s.silva7@ba.estudante.senai.br>
2026-05-02 13:17:39 -0300

    Refactor salary calculation with color output

--- match_salario_hora.py
+++ match_salario_hora.py
@@ -1,13 +1,51 @@
 import os
 os.system("cls")
+from colorama import Fore, Back, Style, init
+import time
 
-def valor(d, nome):
+# Initialize colorama
+init(autoreset=True)
+
+# Atalhos de Estilo
+RESET = Style.RESET_ALL
+BRIGHT = Style.BRIGHT
+DIM = Style.DIM
+
+# Classes
+class F:
+    R   = Fore.RED
+    G   = Fore.GREEN
+    B   = Fore.BLUE
+    Y   = Fore.YELLOW
+    M   = Fore.MAGENTA
+    C   = Fore.CYAN
+    W   = Fore.WHITE
+    K   = Fore.BLACK
+    LR  = Fore.LIGHTRED_EX
+    LG  = Fore.LIGHTGREEN_EX
+    LB  = Fore.LIGHTBLUE_EX
+    LY  = Fore.LIGHTYELLOW_EX
+    LM  = Fore.LIGHTMAGENTA_EX
+    LC  = Fore.LIGHTCYAN_EX
+    LW  = Fore.LIGHTWHITE_EX
+
+# definições
+def valor(d, nome, sb):
+    # cálculo
     horas_trabalhadas = d * 8
-    reais_por_hora = horas_trabalhadas * 25
-    valor_total_reais = f'{reais_por_hora:.2f}'.replace(",", ".")
-    print(f"Salário do(a) funcionário(a) {nome}: R${valor_total_reais}")
+    valor_da_hora = (sb / 30) / 8
+    reais_por_hora = horas_trabalhadas * valor_da_hora
+    valor_total_reais = f'{reais_por_hora:.2f}'.replace(".",",")
+    print(f"Salário do(a) funcionário(a) {nome}: {F.LG}R${RESET}{F.G}{valor_total_reais}")
 
-nome = input("Nome do funcionário: ")
-dias = int(input("Digite quantos dias foram trabalhados no mês: "))
 
-valor(dias, nome)
+# dados
+print(f"{F.B}{BRIGHT}===== Solicitando Dados =====")
+salario_bruto = float(input(f"Valor do salário bruto: {F.LG}R$"))
+nome = input(f"{RESET}Nome do funcionário: {F.LY}")
+dias = int(input(f"{RESET}Digite quantos dias foram trabalhados no mês: {F.LM}"))
+
+# resultado
+os.system("cls")
+print(f"{F.B}===== EXibindo Dados =====")
+valor(dias, nome, salario_bruto)
