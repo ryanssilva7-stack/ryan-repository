9ea80dee5a7e0f5e843afecc0eea857a86d80efd
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-08 16:16:37 -0300

    js feito sem bug

--- relogio/js/script.js
+++ relogio/js/script.js
@@ -13,6 +13,16 @@ function relogio(){
     m = hoje.getMinutes()
     s = hoje.getSeconds()
 
+    if(h < 10){
+        h ='0'+ h
+    }
+    if(m < 10){
+        m ='0'+ m
+    }
+    if(s < 10){
+        s ='0'+ s
+    }
+
     horas.textContent = h
     minutos.textContent = m
     segundos.textContent = s
