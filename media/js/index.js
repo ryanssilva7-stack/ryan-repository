a378a97ad880e984d12f380957f95e7c4680142f
Ryan <ryan.s.silva7@ba.estudante.senai.br>
2026-09-20 14:59:54 -0300

    html e css feitos

---
+++ media/js/index.js
@@ -0,0 +1,11 @@
+// DOM
+const nota1 = document.querySelector('#nota1')
+const nota2 = document.querySelector('#nota2')
+const nota3 = document.querySelector('#nota3')
+const aca
+
+// action
+
+
+// event
+
