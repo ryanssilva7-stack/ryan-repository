5f6890933e0ac096c3aaa01c10afbeefa7502e85
Ryan <ryan.s.silva7@ba.estudante.senai.br>
2026-09-20 17:29:26 -0300

    js bugado

--- media/js/index.js
+++ media/js/index.js
@@ -2,10 +2,31 @@
 const nota1 = document.querySelector('#nota1')
 const nota2 = document.querySelector('#nota2')
 const nota3 = document.querySelector('#nota3')
-const aca
-
-// action
-
+const acao = document.querySelector('acao')
+const media = document.querySelector('media')
+const situacao = document.querySelector('situacao')
 
 // event
+acao.addEventListener('click', calcular)
 
+// action
+function calcular(){
+    n1 = Number(nota1.value)
+    n2 = Number(nota2.value)
+    n3 = Number(nota3.value)
+
+    const Total = [n1, n2, n3]
+    const soma = n1+n2+n3
+    // if (n1 > 10)
+    m = soma / length(total)
+
+    if (m > 5){
+        s = `Aprovado`
+    }
+    else {
+        s = `Reprovado`
+    }
+
+    media.textContent = m
+    situacao.textContent = s
+}
