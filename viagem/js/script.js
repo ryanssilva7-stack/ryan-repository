ed7df01d74acd5959fae0d263e6da2148ace40da
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-21 14:57:04 -0300

    80%

--- viagem/js/script.js
+++ viagem/js/script.js
@@ -1,12 +1,20 @@
 // dom
-km = document.querySelector('#distancia')
-consumo = document.querySelector('#veic_consumo')
-preco = document.querySelector('#preco_combustivel')
-bt = document.querySelector('#bt_calculo') 
-result = document.querySelector('#result') 
+const km = document.querySelector('#distancia')
+const consumo = document.querySelector('#veic_consumo')
+const preco = document.querySelector('#preco_combustivel')
+const bt = document.querySelector('#bt_calculo') 
+const result = document.querySelector('#result') 
 
 // event
 bt.addEventListener('click', calculo)
 
 // action
-calculo
+function calculo(){
+    d = Number(km.value)
+    c = Number(consumo.value)
+    p = Number(preco.value)
+
+    r = (d/c)*p
+
+    result.textContent = `R$ ${r.toFixed(2)}`
+}
